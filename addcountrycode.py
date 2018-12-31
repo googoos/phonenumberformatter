281434a5fc8ab63f5a7b60574919800a8c299f1d
Gugus A. Sakti <12741377+googoos@users.noreply.github.com>
2018-12-31 14:07:25 +0700

    Some enhancement ideas

--- addcountrycode.py
+++ addcountrycode.py
@@ -1,8 +1,22 @@
 import csv
 
+"""
+Convert phone number data to international format.
+"""
+
+# Should: Read csv with 2 colums data (country code & phone number)
 with open('input.csv') as inf:
     reader = csv.reader(inf.readlines())
 
+# Should: Check phone number start with '0' and save it somewhere.
+
+# Should: Check phone number not start with '0' and save it somewhere.
+
+# Should: Check if phone number not start with '0' is less then X character and save it somewhere.
+
+# Should: add country code.
+
+# Should: write formated data to new file.
 with open('output.csv', 'w', newline='') as outf:
     writer = csv.writer(outf)
     for line in reader:
